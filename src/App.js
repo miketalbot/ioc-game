4574fafbd3f3dd91c487b2dfc212e414a51f328d
Mike Talbot <38054740+miketalbot@users.noreply.github.com>
2020-06-26 16:55:58 +0100

    update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react"
 import "./styles.css"
 import { raise, raiseAsync } from "./lib/event-bus"
 import { GameSurface } from "./game/game-surface"
-import { parse} from "query-string"
+import { parse } from "query-string"
 
 //Core requirements for the game / menus etc
 import "./game"
@@ -21,23 +21,23 @@ import "./powered-by.js"
 // missing
 import "./game/levels/level-definitions"
 
-if(!window.location.search) {
-    window.location.search = "load=music.js&load=ambient-sounds.js&load=bubble-sounds.js&load=apple-sounds.js&load=level-sounds.js"
+if (!window.location.search) {
+    window.location.search =
+        "load=music.js&load=ambient-sounds.js&load=bubble-sounds.js&load=apple-sounds.js&load=level-sounds.js"
 }
 
 export default function App() {
     const [ready, setReady] = React.useState(false)
     const loaded = React.useRef(true)
-    React.useEffect(()=>{
+    React.useEffect(() => {
         start().catch(console.error)
-        return ()=>{
+        return () => {
             loaded.current = false
         }
     }, [])
-    if(ready) {
+    if (ready) {
         const [uiElements] = raise("ui", [])
         return (
-
             <div className="App">
                 <GameSurface>{uiElements}</GameSurface>
             </div>
@@ -51,9 +51,8 @@ export default function App() {
         await raiseAsync("initializeGame", parameters)
         await raiseAsync("postInitializeGame", parameters)
         await raiseAsync("gameReady", parameters)
-        if(loaded.current) {
+        if (loaded.current) {
             setReady(true)
         }
     }
-
 }
